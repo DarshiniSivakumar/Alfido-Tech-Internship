a640b78aa529ff674c2dd1ab5fdce45090b7d5b9
DarshiniSivakumar <darshinisivakumar001@gmail.com>
2025-11-09 12:38:09 +0530

    Rename file to iris_classifiation(TASK-1).py

--- iris_classifiation(TASK-1).py
+++ iris_classifiation(TASK-1).py
@@ -3,6 +3,16 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisiticRegression
 from sklearn.metrics import acuracy_score,classification_report
+import zipfile
+zip_path = r"C:\Users\Darshini\Downloads\archive (4).zip"
+extract_to = r"C:\Users\Darshini\Downloads\iris_data"
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_to)
+print("✅ Files extracted to:", extract_to)
+csv_path = extract_to + r"\iris.csv"  # change if filename differs
+df = pd.read_csv(csv_path)
+print("✅ Dataset loaded successfully!\n")
+print(df.head())
 df=pd.read_csv("Iris.csv")
 X=df.drop("species",axis=1)
 y=df["species"]
@@ -19,3 +29,4 @@ y_pred_lr=lr_model.predict(X_test)
 print("Logistic Regression")
 print("ACCURACY:",accuracy_score(y_test,y_pred)
 print("Classification:",classification_report(y_test,y_pred)
+
